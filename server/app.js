2cfc92578025a05cfc43e16d858dd418057cd235
wilson <wilson@wilsondeMacBook-Pro-2.local>
2016-10-29 18:21:48 +0800

    update

--- server/app.js
+++ server/app.js
@@ -3,10 +3,6 @@
 const express   = require('express')
 const app       = express()
 
-// RESOURCES
-const imagebox    = require('./routes/imagebox')
-const api         = require('./routes/api') 
-
 //
 app.use(function(req, res, next) {
     res.set({
@@ -16,9 +12,6 @@ app.use(function(req, res, next) {
 })
 app.use(express.static(__dirname + '/public'));
 
-// APIS
-app.get('/imagebox', imagebox.upload)
-app.get('/readapi', api.requestApi)
 app.get('*', (req, res) => {res.sendFile(__dirname + '/public/index.html')})
 
 
